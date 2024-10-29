a01373f9787064dc6756672e05054632bc84962c
Nick Larson <nick.larson@improving.com>
2024-10-29 09:25:12 -0500

    cleanup

--- data/loadProducts.py
+++ data/loadProducts.py
@@ -7,9 +7,9 @@ from kafka import KafkaProducer
 # Run - `terraform output resource-ids` in terraform/demo-infrastructure to expose these values.
 # 
 # Then:
-# export KAFKA_BOOTSTRAP_URL=<your boostrap url>
-# export KAFKA_API_KEY_ID=<your api key id>
-# export KAFKA_API_KEY_SECRET=<your api key secret>
+# export DEMO_CLUSTER_BOOTSTRAP=<your boostrap url>
+# export PRODUCT_API_KEY_ID=<your api key id>
+# export PRODUCT_API_KEY_SECRET=<your api key secret>
 #
 # Then:
 # python loadProducts.py
@@ -17,12 +17,12 @@ from kafka import KafkaProducer
 # ########################################
 
 producer = KafkaProducer(
-    bootstrap_servers= os.environ.get("KAFKA_BOOTSTRAP_URL", "Set The Environment Var"),  # Environment Var For kafka bootstrap url (terraform output resource-ids)
+    bootstrap_servers= os.environ.get("DEMO_CLUSTER_BOOTSTRAP", "Set The Environment Var"),  # Environment Var For kafka bootstrap url (terraform output resource-ids)
     security_protocol='SASL_SSL',
     sasl_mechanism='PLAIN',
 
-    sasl_plain_username= os.environ.get("KAFKA_API_KEY_ID", "Set The Environment Var"),  # Environment Var For kafka API Key Id (terraform output resource-ids)
-    sasl_plain_password= os.environ.get("KAFKA_API_KEY_SECRET", "Set The Environment Var"),  # Environment Var For kafka API Key Secret (terraform output resource-ids)
+    sasl_plain_username= os.environ.get("PRODUCT_API_KEY_ID", "Set The Environment Var"),  # Environment Var For kafka API Key Id (terraform output resource-ids)
+    sasl_plain_password= os.environ.get("PRODUCT_API_KEY_SECRET", "Set The Environment Var"),  # Environment Var For kafka API Key Secret (terraform output resource-ids)
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
--- terraform/demo-infrastructure/outputs.tf
+++ terraform/demo-infrastructure/outputs.tf
@@ -13,8 +13,8 @@ output "resource-ids" {
   **************
 
   "bootstrap-servers": "${module.demo_environment.confluent_kafka_cluster.bootstrap_endpoint}"
-  "api-key-id":  "${module.demo_environment.data_steward_api_key.id}"
-  "api-key-secret": "${module.demo_environment.data_steward_api_key.secret}"
+  "api-key-id":  "${module.demo_environment.cluster_admin_api_key.id}"
+  "api-key-secret": "${module.demo_environment.cluster_admin_api_key.secret}"
 
   "schema-reg-url": "${module.demo_environment.schema_registry_cluster.rest_endpoint}"
   "schema-reg-api-key-id": "${module.demo_environment.data_steward_api_key.id}"
@@ -26,6 +26,14 @@ output "resource-ids" {
   **************
 
   > confluent flink shell --compute-pool ${module.demo_environment.flink_compute_pool_id} --environment ${module.demo_environment.id}
+
+  **************  **************  **************  **************  **************  **************
+  PRODUCT PRODUCER SETTINGS
+  Paste this line to set all the ENV variables to run the product producer.
+  **************  **************  **************  **************  **************  **************
+
+  export DEMO_CLUSTER_BOOTSTRAP=${trimprefix(module.demo_environment.confluent_kafka_cluster.bootstrap_endpoint, "SASL_SSL://")}; export PRODUCT_API_KEY_ID=${module.demo_environment.cluster_admin_api_key.id};export PRODUCT_API_KEY_SECRET=${module.demo_environment.cluster_admin_api_key.secret}
+
 
   EOT
 
